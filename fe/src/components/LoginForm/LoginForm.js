70d2f8e302133be72307bb98f2357ff9501e211d
Mantas M <mantasef@gmail.com>
2022-09-30 00:12:31 +0300

    Login&Register Forms

---
+++ fe/src/components/LoginForm/LoginForm.js
@@ -0,0 +1,38 @@
+import css from './LoginForm.module.css';
+
+import { useFormik } from 'formik';
+
+const initValues = {
+  email: '',
+  password: '',
+};
+
+function LoginForm() {
+  const formik = useFormik({
+    initialValues: initValues,
+    onSubmit: (values) => {
+      console.log('values ===', values);
+    },
+  });
+
+  return (
+    <div className={css['form-container']}>
+      <h1>Login</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className={css['form-group']}>
+          <label htmlFor='email'>Email</label>
+          <input onChange={formik.handleChange} value={formik.values.email} type='email' id='email' name='email' />
+        </div>
+        <div className={css['form-group']}>
+          <label htmlFor='password'>Password</label>
+          <input onChange={formik.handleChange} value={formik.values.password} type='password' id='password' name='password' />
+        </div>
+        <button type='submit' className={css.button}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
---
+++ fe/src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,49 @@
+import css from './RegisterForm.module.css';
+
+import { useFormik } from 'formik';
+
+const initValues = {
+  email: '',
+  password: '',
+  repeatPassword: '',
+};
+
+function RegisterForm() {
+  const formik = useFormik({
+    initialValues: initValues,
+    onSubmit: (values) => {
+      console.log('values ===', values);
+    },
+  });
+
+  return (
+    <div className={css['form-container']}>
+      <h1>Registration</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className={css['form-group']}>
+          <label htmlFor='email'>Email</label>
+          <input onChange={formik.handleChange} value={formik.values.email} type='email' id='email' name='email' />
+        </div>
+        <div className={css['form-group']}>
+          <label htmlFor='password'>Password</label>
+          <input onChange={formik.handleChange} value={formik.values.password} type='password' id='password' name='password' />
+        </div>
+        <div className={css['form-group']}>
+          <label htmlFor='repeatPassword'>Repeat password</label>
+          <input
+            onChange={formik.handleChange}
+            value={formik.values.repeatPassword}
+            type='password'
+            id='repeatPassword'
+            name='repeatPassword'
+          />
+        </div>
+        <button type='submit' className={css.button}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
