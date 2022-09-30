a19098762a304e619b8e6479da609c9714d36a30
Mantas M <mantasef@gmail.com>
2022-09-30 16:46:48 +0300

    isUserLoggedIn, logout, userEmail

--- fe/src/components/Header/Header.js
+++ fe/src/components/Header/Header.js
@@ -1,25 +1,53 @@
 import css from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import { useAuthCtx } from '../../store/AuthContext';
+import toast from 'react-hot-toast';
 
 function Header(props) {
+  const { isUserLoggedIn, logout, userEmail } = useAuthCtx();
   return (
-    <header className={css.header}>
-      <NavLink to='/'>
-        <img className={css.img} src='../img/book.png' alt='logoNav' />
-      </NavLink>
-      <nav>
-        <NavLink className={css['nav-link']} to={'/'}>
-          Home
+    <header>
+      <nav className={css.header}>
+        <NavLink to='/'>
+          <img className={css.img} src='../img/book.png' alt='logoLibrary' />
         </NavLink>
-        <NavLink className={css['nav-link']} to={'/books'}>
-          Books
-        </NavLink>
-        <NavLink className={css['nav-link']} to={'/login'}>
-          Login
-        </NavLink>
-        <NavLink className={css['nav-link']} to={'/register'}>
-          Register
-        </NavLink>
+        <div className={css['nav-container']}>
+          {/* <NavLink className={css['nav-link']} to={'/'}>
+            Home
+          </NavLink> */}
+          <NavLink className={css['nav-link']} to={'/books'}>
+            Books
+          </NavLink>
+          {isUserLoggedIn && (
+            <>
+              {/* <div className={css['nav-control-display']}></div> */}
+              {<p className={css.email}>You are logged in as: {userEmail}</p>}
+              <NavLink
+                onClick={() => {
+                  logout();
+                  isUserLoggedIn ? toast.success('You are logged out.') : toast.error('Error in logout.');
+                }}
+                className={css['nav-link']}
+                to={'/login'}
+              >
+                Logout
+              </NavLink>
+            </>
+          )}
+        </div>
+
+        {!isUserLoggedIn && (
+          <>
+            <div className={css['nav-control-display']}>
+              <NavLink className={css['nav-link']} to={'/login'}>
+                Login
+              </NavLink>
+              <NavLink className={css['nav-link']} to={'/register'}>
+                Register
+              </NavLink>
+            </div>
+          </>
+        )}
       </nav>
     </header>
   );
